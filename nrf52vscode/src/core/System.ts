0d8077c75d67915723f0e491a2d2fd32c6f076d3
Piotr Muras <piotrekm7@gmail.com>
2020-04-17 11:20:53 +0200

    changes in choosing folder for new project

--- nrf52vscode/src/core/System.ts
+++ nrf52vscode/src/core/System.ts
@@ -5,35 +5,27 @@ export class System {
     /*
     System class is used by vs code extension. It creates appropriate ProgrammingSystem instance and calls its methods.
      */
-    static openDialogForFolder(): Promise<vscode.Uri> {
+
+    static newProject(): void {
         /*
-        Gui function which enables user choosing folder for new or existing project.
+        Creates new project.
+        Enables user choosing folder for new project.
+        Should ask user about project type. (Not implemented)
          */
-        const options: vscode.OpenDialogOptions = {
+        vscode.window.showOpenDialog({
             canSelectFiles: false,
             canSelectFolders: true,
             canSelectMany: false
-        };
-        const result: vscode.Uri[] | undefined = vscode.window.showOpenDialog(Object.assign(options));
-        if (result && result.length) {
-            return Promise.resolve(result[0]);
-        } else {
-            return Promise.reject();
-        }
-    }
-
-    static newProject(): boolean {
-        /*
-        Creates new project.
-        Should ask user about project type. (Not implemented)
-         */
-        const result: vscode.Uri = System.openDialogForFolder();
-        if (result && result.fsPath) {
-            vscode.commands.executeCommand('vscode.openFolder', result);
-            // TODO should ask user about project type, it's problematic though, so I hardcoded 52840 for now
-            const system = getSystem(Systems.NRF52840);
-            return system.newProjectTask(result.fsPath);
-        }
-        return false;
+        }).then(folders => {
+            if (folders && folders[0]) {
+                vscode.commands.executeCommand("vscode.openFolder", folders[0]);
+                // TODO should ask user about project type, it's problematic though, so I hardcoded 52840 for now
+                const system = getSystem(Systems.NRF52840);
+                if(system.newProjectTask(folders[0].fsPath)){
+                    console.log('New project created!');
+                }
+            }
+        })
+        console.log('A problem occured when creating new project!');
     }
 }
