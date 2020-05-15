74b329fa7580c8d2a893d15c952188e12974e1f6
Adam Grzybowski <aptgrzybowski@gmail.com>
2020-05-15 02:53:14 +0200

    bugfix

--- VsCodeExtension/src/extension.ts
+++ VsCodeExtension/src/extension.ts
@@ -4,8 +4,8 @@ import * as Webviews from "./frontend/Webviews";
 
 export function activate(context: vscode.ExtensionContext) {
 
-    let newProject = vscode.commands.registerCommand('LowLevelVsCode.NewProject', Webviews.projectSettings(context));
-    // let newProject = vscode.commands.registerCommand('LowLevelVsCode.NewProject', Webviews.newProject(context));
+    // let newProject = vscode.commands.registerCommand('LowLevelVsCode.NewProject', Webviews.projectSettings(context));
+    let newProject = vscode.commands.registerCommand('LowLevelVsCode.NewProject', Webviews.newProject(context));
     let generateMakefile = vscode.commands.registerCommand('LowLevelVsCode.generateMakefile', System.generateMakefile);
     context.subscriptions.push(newProject, generateMakefile);
 }
