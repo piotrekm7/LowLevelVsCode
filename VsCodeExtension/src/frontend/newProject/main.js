0e1973aabe6ab28c06de3b5d0233febffba05736
Piotr Muras <piotrekm7@gmail.com>
2020-05-15 08:18:30 +0200

    minor changes

--- VsCodeExtension/src/frontend/newProject/main.js
+++ VsCodeExtension/src/frontend/newProject/main.js
@@ -1,23 +1,24 @@
 const vscode = acquireVsCodeApi();
+
 function createOptions(arr) {
-	console.log(arr);
-	for (const name of arr) {
-		const option = document.createElement("option");
-		option.value = name;
-		option.innerText = name;
-		const select = document.getElementById("select-system-type");
-		select.appendChild(option);
-	}
+    console.log(arr);
+    for (const name of arr) {
+        const option = document.createElement("option");
+        option.value = name;
+        option.innerText = name;
+        const select = document.getElementById("select-system-type");
+        select.appendChild(option);
+    }
 };
 
 window.addEventListener('message', event => {
-	console.log(event.data);
-	createOptions(event.data);
+    console.log(event.data);
+    createOptions(event.data);
 });
 
 document.getElementById("button-send-type").addEventListener('click', e => {
-	vscode.postMessage({type: 'submit', value: document.getElementById('select-system-type').value});
-	console.log('wysylanie...');
+    vscode.postMessage({type: 'submit', value: document.getElementById('select-system-type').value});
+    console.log('wysylanie...');
 });
 
 vscode.postMessage({type: 'loaded'});
