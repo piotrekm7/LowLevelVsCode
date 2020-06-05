3b192020e7de1767279efb3580447299e1e0e41d
Piotr Muras <piotrekm7@gmail.com>
2020-06-05 11:55:01 +0200

    makefile generation - work in progress

--- VsCodeExtension/src/core/NRF.ts
+++ VsCodeExtension/src/core/NRF.ts
@@ -7,58 +7,66 @@ export abstract class NRF extends Embedded {
 
     protected addSettings(): void {
         /*
-        Adds common settings for all NRF devices.
-         */
+            Adds common settings for all NRF devices.
+             */
         super.addSettings();
-        this.settings.set('GNU_GCC', 'C:/Program Files (x86)/GNU Tools ARM Embedded/7 2018-q2-update/');
-        this.settings.set('nRF_SDK', 'C:\\Users\\piotr\\Desktop\\ABB\\nrf52incode\\nRF5_SDK_15.3.0_59ac345\\nRF5_SDK_15.3.0_59ac345');
-        this.settings.set('JLinkGDBServer', 'C:/Program Files (x86)/SEGGER/JLink/JLinkGDBServerCL.exe');
+        this.settings.set(
+            "GNU_GCC",
+            "C:/Program Files (x86)/GNU Tools ARM Embedded/7 2018-q2-update/"
+        );
+        this.settings.set(
+            "nRF_SDK",
+            "C:\\Users\\piotr\\Desktop\\ABB\\nrf52incode\\nRF5_SDK_15.3.0_59ac345\\nRF5_SDK_15.3.0_59ac345"
+        );
+        this.settings.set(
+            "JLinkGDBServer",
+            "C:/Program Files (x86)/SEGGER/JLink/JLinkGDBServerCL.exe"
+        );
     }
 
     protected getCompilerPath(): string {
         /*
-        Returns compiler path to NRF devices.
-         */
-        return path.join(this.settings.get('GNU_GCC')!, 'bin/arm-none-eabi-gcc');
+            Returns compiler path to NRF devices.
+             */
+        return path.join(this.settings.get("GNU_GCC")!, "bin/arm-none-eabi-gcc");
     }
 
     protected getIncludePath(): Array<string> {
         /*
-        Returns include Path for NRF devices.
-         */
+            Returns include Path for NRF devices.
+             */
         return [
-            '${workspaceFolder}/**',
-            path.join(this.settings.get('GNU_GCC')!, 'arm-none-eabi/include'),
-            path.join(this.settings.get('nRF_SDK')!, 'modules/**'),
-            path.join(this.settings.get('nRF_SDK')!, 'components/**')
+            "${workspaceFolder}/**",
+            path.join(this.settings.get("GNU_GCC")!, "arm-none-eabi/include"),
+            path.join(this.settings.get("nRF_SDK")!, "modules/**"),
+            path.join(this.settings.get("nRF_SDK")!, "components/**"),
         ];
     }
-
 
     // TODO Some of these are device specific, should be moved to subclasses
     protected getDefines(): Array<string> {
         /*
-        List of C defines to be set up for all NRF devices.
-         */
+            List of C defines to be set up for all NRF devices.
+             */
         return [
-            'CONFIG_GPIO_AS_PINRESET',
-            'INITIALIZE_USER_SECTIONS',
-            'FLOAT_ABI_HARD',
-            'NRF_SD_BLE_API_VERSION=6',
-            'S140',
-            'SOFTDEVICE_PRESENT',
-            'SWI_DISABLE0',
-            '_DEBUG',
-            'UNICODE',
-            '_UNICODE',
-            this.getDeviceSignature()
+            "CONFIG_GPIO_AS_PINRESET",
+            "INITIALIZE_USER_SECTIONS",
+            "FLOAT_ABI_HARD",
+            "NRF_SD_BLE_API_VERSION=6",
+            "S140",
+            "SOFTDEVICE_PRESENT",
+            "SWI_DISABLE0",
+            "_DEBUG",
+            "UNICODE",
+            "_UNICODE",
+            this.getDeviceSignature(),
         ];
     }
 
     protected getDebuggerConfiguration(): any {
         /*
-        Returns cortex-debug configuration used for debugging NRF devices via J-Link.
-         */
+            Returns cortex-debug configuration used for debugging NRF devices via J-Link.
+             */
         return {
             type: "cortex-debug",
             request: "launch",
@@ -66,16 +74,16 @@ export abstract class NRF extends Embedded {
             cwd: "${workspaceRoot}",
             // TODO check this path
             executable: "${workspaceRoot}/_build/nrf52840_xxaa.out",
-            serverpath: this.settings.get('JLinkGDBServer'),
+            serverpath: this.settings.get("JLinkGDBServer"),
             servertype: "jlink",
             device: this.getDeviceSignature(),
             interface: "swd",
             serialNumber: "",
             runToMain: true,
-            armToolchainPath: this.settings.get('GNU_GCC') + '/bin',
+            armToolchainPath: this.settings.get("GNU_GCC") + "/bin",
             preLaunchTask: "build",
             // TODO should depend on specific device
-            svdFile: "${workspaceRoot}/nrf52840.svd"
+            svdFile: "${workspaceRoot}/nrf52840.svd",
         };
     }
 
@@ -83,31 +91,33 @@ export abstract class NRF extends Embedded {
 
     protected getBuildTarget(): string {
         /*
-        Returns build target for compiler.
-         */
+            Returns build target for compiler.
+             */
         return this.getDeviceSignature();
     }
 
     protected getIncludeFolders(): string {
         /*
-        Runs DependencyAnalyzer for generating list of include folders.
-        Transform list of folders to appropriately formatted string.
-         */
+            Runs DependencyAnalyzer for generating list of include folders.
+            Transform list of folders to appropriately formatted string.
+             */
         if (this.dependencyAnalyzer === undefined) {
             this.createDependencyAnalyzer();
         }
-        return this.dependencyAnalyzer!.getListOfIncludeFolders().join(' \\\n');
+        this.dependencyAnalyzer?.performScanning();
+        return this.dependencyAnalyzer!.getListOfIncludeFolders().join(" \\\n");
     }
 
     protected getSourceFiles(): string {
         /*
-        Runs DependencyAnalyzer for generating list of source files.
-        Transform list of source files to appropriately formatted string.
-         */
+            Runs DependencyAnalyzer for generating list of source files.
+            Transform list of source files to appropriately formatted string.
+             */
         if (this.dependencyAnalyzer === undefined) {
             this.createDependencyAnalyzer();
         }
-        return this.dependencyAnalyzer!.getListOfSourceDependencies().join(' \\\n');
+        this.dependencyAnalyzer?.performScanning();
+        return this.dependencyAnalyzer!.getListOfSourceDependencies().join(" \\\n");
     }
 
     private createDependencyAnalyzer(): void {
@@ -117,8 +127,8 @@ export abstract class NRF extends Embedded {
     //TODO Consider allowing user to edit list of lib files
     protected getLibFiles(): string {
         /*
-        Returns list of additional libraries required by project.
-         */
+            Returns list of additional libraries required by project.
+             */
         return "";
     }
 }
